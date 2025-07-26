a9abebc96adc6d237a0c97a11745d2b6d269e432
your_username <kohseukim@gmail.com>
2025-07-26 10:25:06 +0800

    docs: document interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,12 +3,24 @@ export interface IValidatorType {
 	 * the input string to validate
 	 */
 	inputString: string;
+	/**
+	 * to validate for which ID type, NRIC or FIN or both
+	 */
 	type: IDType;
 }
 
 export interface ICheckSumChecker {
+	/**
+	 * takes in the prefix of NRIC / FIN
+	 */
 	prefix: NRICPrefixType | FINPrefixType | PrefixType;
+	/**
+	 * refers to the numbers between the prefix and the checksum character
+	 */
 	idNumbers: number[];
+	/**
+	 * to validate for which ID type, NRIC or FIN or both
+	 */
 	type: IDType;
 }
 
@@ -16,6 +28,7 @@ export interface IGenerateID {
 	type: Exclude<IDType, "BOTH">;
 }
 
+// types required for the prefixes
 export const NRICPrefixes = ["S", "T"] as const;
 export type NRICPrefixType = (typeof NRICPrefixes)[number];
 
